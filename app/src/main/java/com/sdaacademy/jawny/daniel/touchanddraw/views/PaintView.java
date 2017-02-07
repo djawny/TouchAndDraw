45f2458eef5e8e14f9313ef712be4def3d7ea3be
djawny <djawny@op.pl>
2017-02-07 18:23:53 +0100

    zmiana 07/02

---
+++ app/src/main/java/com/sdaacademy/jawny/daniel/touchanddraw/views/PaintView.java
@@ -0,0 +1,42 @@
+package com.sdaacademy.jawny.daniel.touchanddraw.views;
+
+import android.content.Context;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class PaintView extends View implements View.OnTouchListener {
+    public PaintView(Context context) {
+        super(context);
+        init();
+    }
+
+    public PaintView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public PaintView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public PaintView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
+
+    private void init() {
+        setOnTouchListener(this);
+    }
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        return false;
+    }
+}
