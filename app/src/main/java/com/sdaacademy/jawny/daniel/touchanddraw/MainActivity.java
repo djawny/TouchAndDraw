8db1261cd8c8771d765372367b49187e34a0144f
djawny <djawny@op.pl>
2017-02-07 20:10:27 +0100

    zmiana 07/02

--- app/src/main/java/com/sdaacademy/jawny/daniel/touchanddraw/MainActivity.java
+++ app/src/main/java/com/sdaacademy/jawny/daniel/touchanddraw/MainActivity.java
@@ -1,13 +1,26 @@
 package com.sdaacademy.jawny.daniel.touchanddraw;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DialogInterface.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AlertDialog alertDialog = new AlertDialog.Builder(this)
+                .setMessage("Cześć, jestem AlertDialog")
+                .setCancelable(false)
+                .setPositiveButton("Super", null)
+                .create();
+        alertDialog.show();
+    }
+
+    @Override
+    public void onClick(DialogInterface dialog, int which) {
+
     }
 }
